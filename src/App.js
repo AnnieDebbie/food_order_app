1395c53bc6d78d6fbcf03acd43887549274c2246
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-03-24 18:35:55 +0100

    - Create showFormHandler and hideFormHandler for order form - Pass formHandler functions as props

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import MealOrderForm from "./components/Order/MealOrderForm";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
+  const [formIsShown, setFormIsShown] = useState(false);
   const showCartHandler = () => {
     setCartIsShown(true);
   };
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
+  const showFormHandler = () => {
+    setFormIsShown(true);
+    setCartIsShown(false);
+  };
+  const hideFormHandler = () => {
+    setFormIsShown(false);
+  };
+
   return (
     <CartProvider>
-      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      {cartIsShown && (
+        <Cart onCloseCart={hideCartHandler} onShowForm={showFormHandler} />
+      )}
+      {formIsShown && <MealOrderForm onCloseForm={hideFormHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
