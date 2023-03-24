bac32f453a83c64f26998bc5d6d4370c15534410
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-03-24 18:30:31 +0100

    Listen for onBlur and onChange events on custom Input

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,9 +2,14 @@ import React from "react";
 import classes from "./Input.module.css";
 const Input = React.forwardRef((props, ref) => {
   return (
-    <div className={classes.input}>
+    <div className={`${classes.input} ${props.className}`}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input ref={ref} {...props.input} />
+      <input
+        className={props.inputClass}
+        onBlur={props.onBlur}
+        onChange={props.onChange}
+        ref={ref}
+      />
     </div>
   );
 });
